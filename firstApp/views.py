29f9944dfb348f9fbc011dc2f747d44808b4f899
paulsmalze <waffwegs@yahoo.com>
2021-04-17 10:16:46 -0500

    added a new method named new

--- firstApp/views.py
+++ firstApp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("placeholder to later display a list of all blogs")
+    
+def new(request):
+    return HttpResponse("placeholder to display a new form to create a new blog")
