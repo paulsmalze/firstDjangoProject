20ebdff4e04ee4d104546487c1b6f8c01973629f
paulsmalze <waffwegs@yahoo.com>
2021-04-17 10:29:49 -0500

    added redirect

--- firstApp/views.py
+++ firstApp/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse,redirect
 
 # Create your views here.
 def index(request):
     return HttpResponse("placeholder to later display a list of all blogs")
-    
+
 def new(request):
     return HttpResponse("placeholder to display a new form to create a new blog")
+
+def create(request):
+    return redirect("/")
